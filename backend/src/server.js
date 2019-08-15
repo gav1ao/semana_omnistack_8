7c3877249dd8110d43256a77ced6e0a5a33de017
Vítor Gavião <vigal.jan@gmail.com>
2019-08-14 23:27:56 -0300

    Adição de variáveis de ambiente ao projeto.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,6 +9,8 @@ const routes = require('./routes');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3333;
 
 const connectedUsers = {};
 
@@ -18,7 +22,7 @@ io.on('connection', socket => {
     connectedUsers[user] = socket.id;
 });
 
-mongoose.connect(`mongodb+srv://omnistack:omnistack@omnistack8-9j1vf.mongodb.net/omnistack8?retryWrites=true&w=majority`, {
+mongoose.connect(process.env.MONGO_DB_URL, {
     useNewUrlParser: true
 });
 
@@ -34,4 +38,6 @@ app.use(express.json());
 app.use(routes);
 
 
-server.listen(3333);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
